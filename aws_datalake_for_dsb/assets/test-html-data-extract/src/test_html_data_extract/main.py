4371d518eaa360a42c61ed1239ffa5a950c35bec
bitbauer <4582513+bitbauer@users.noreply.github.com>
2024-12-26 21:04:37 +0100

    ADD first working code to parse html data from dsb

--- aws_datalake_for_dsb/assets/test-html-data-extract/src/test_html_data_extract/main.py
+++ aws_datalake_for_dsb/assets/test-html-data-extract/src/test_html_data_extract/main.py
@@ -1,9 +1,27 @@
 import os
+import re
 import pandas as pd
+from pyquery import PyQuery as pq
+
+
+def parse_dsb_html_file(filename):
+  print(filename)
+
+  html = pq(filename=filename, encoding='iso-8859-1')
+  day = html("div.mon_title").text()
+
+  tables = pd.read_html(filename)
+
+  header = tables[0].iat[0, -1]
+  last_update = re.split('[Ss]tand:', header)
+
+  tables[1].insert(0, "Stand", last_update[1].strip())
+  tables[1].insert(0, "Datum", day.split()[0])
+  tables[1].insert(0, "Wochentag", day.split()[1])
+
+  return tables[1]
 
 path = "./temp/20241010-050032_plans/"
-for filename in os.listdir(path):
-  dataframes = pd.read_html(os.path.join(path, filename))
+results = [parse_dsb_html_file(os.path.join(path, filename)) for filename in os.listdir(path)]
 
-for df in dataframes:
-  print(df.to_string(index=False))
+print(pd.concat(results, ignore_index=True, sort=False).sort_values(['Datum', 'Klasse(n)'], ascending = [True, True]))
