38430fa11111bfb0a68a7b2cacfb1509862d7e50
bitbauer <4582513+bitbauer@users.noreply.github.com>
2024-12-30 00:29:24 +0100

    ADD code for converter stack without experimental SnsToSqs and working cdk synth

--- aws_datalake_for_dsb/dsb_to_parquet_converter_stack.py
+++ aws_datalake_for_dsb/dsb_to_parquet_converter_stack.py
@@ -9,10 +9,11 @@ from aws_cdk import (
     aws_logs,
     aws_s3,
     aws_sns,
-    aws_sqs
+    aws_sns_subscriptions,
+    aws_sqs,
 )
 from constructs import Construct
-from aws_solutions_constructs.aws_sns_sqs import SnsToSqs
+
 
 class DsbToParquetConverterStack(Stack):
 
@@ -24,6 +25,8 @@ class DsbToParquetConverterStack(Stack):
                  s3_parquet_prefix: str,
                  **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        LAMBDA_TIMEOUT = 900
 
         # The parameters defined for your stack
 
@@ -37,38 +40,30 @@ class DsbToParquetConverterStack(Stack):
 
         # The code that defines your stack goes here
 
-        self._sns_to_sqs = SnsToSqs(
-            scope=self,
-            id="SnsToSQS",
-            existing_topic_obj=sns_download_topic,
-            encrypt_topic_with_cmk=False,
-            encrypt_queue_with_cmk=False,
-            queue_props=aws_sqs.QueueProps(
-                # content_based_deduplication=None,
-                # data_key_reuse=None,
-                # dead_letter_queue=None,
-                # deduplication_scope=None,
-                # delivery_delay=None,
-                # encryption=None,
-                # encryption_master_key=None,
-                # enforce_ssl=None,
-                # fifo=None,
-                # fifo_throughput_limit=None,
-                # max_message_size_bytes=None,
-                # queue_name=None,
-                # receive_message_wait_time=None,
-                # redrive_allow_policy=None,
-                # removal_policy=None,
-                # retention_period=None,
-                visibility_timeout=Duration.seconds(5400)),
-            enable_encryption_with_customer_managed_key=False
+        self._dead_letter_queue = aws_sqs.Queue(
+            self,
+            id="DLQ",
+            retention_period=Duration.days(7)
         )
+        self._download_queue = aws_sqs.Queue(
+            self,
+            id="SQS",
+            dead_letter_queue=aws_sqs.DeadLetterQueue(max_receive_count=1, queue=self._dead_letter_queue),
+            visibility_timeout = Duration.seconds(LAMBDA_TIMEOUT * 6)
+        )
+
+        sqs_subscription = aws_sns_subscriptions.SqsSubscription(
+            self._download_queue,
+            raw_message_delivery=True
+        )
+        sns_download_topic.add_subscription(sqs_subscription)
+
 
         # Sync lambda function
         self._function = aws_lambda_python.PythonFunction(
             scope=self,
             id='Lambda',
-            entry='./aws_datalake_for_dsb/assets/dsb_to_parquet_converter_lambda',  # Path to function code
+            entry='./aws_datalake_for_dsb/assets/dsb_to_parquet_converter_lambda/src',  # Path to function code
             description='Convert files on S3 from DSB html to parquet',
             environment=dict(
                 S3_BUCKET_NAME=s3_download_bucket.bucket_name,
@@ -78,13 +73,15 @@ class DsbToParquetConverterStack(Stack):
             ),
             handler='lambda_handler',
             index='handler.py',
+            events=[aws_lambda_event_sources.SqsEventSource(self._download_queue)],
+            dead_letter_queue_enabled=True,
+            dead_letter_queue=self._dead_letter_queue,
             log_retention=aws_logs.RetentionDays.ONE_WEEK,
             max_event_age=Duration.hours(1),
             retry_attempts=2,
             runtime=aws_lambda.Runtime.PYTHON_3_9,
-            timeout=Duration.seconds(900),
+            timeout=Duration.seconds(LAMBDA_TIMEOUT),
             tracing=aws_lambda.Tracing.DISABLED,
         )
 
-        self._bucket.grant_read_write(self._function.grant_principal)
-        self._function.add_event_source(aws_lambda_event_sources.SqsEventSource(self._sns_to_sqs.sqs_queue))
+        s3_download_bucket.grant_read_write(self._function.grant_principal)
