47979aa7b1e7715603bad4ecf94bd46adc482b27
bitbauer <4582513+bitbauer@users.noreply.github.com>
2024-12-31 01:27:47 +0100

    ADD more code for generate parquet and ETL example queries to extend and normalize data with AWS Glue and Athena

--- aws_datalake_for_dsb/assets/dsb_to_parquet_converter_lambda/main.py
+++ aws_datalake_for_dsb/assets/dsb_to_parquet_converter_lambda/main.py
@@ -1,8 +1,47 @@
 import os
+import traceback
 from datetime import datetime
 import re
 import pandas as pd
 from pyquery import PyQuery as pq
+
+##############################################################
+# Sync local temp with S3 data
+##############################################################
+
+# mkdir -p ./temp/download
+# aws sso login --profile stefan-ohs
+# aws s3 sync s3://dsb-data-813986443542/download/ ./temp/download/ --profile stefan-ohs
+# aws s3 sync ./temp/parquet/ s3://dsb-data-813986443542/parquet/ --profile stefan-ohs
+
+##############################################################
+# SQL for crawled Athena table
+##############################################################
+# select datum as datum2, max(parse_datetime(stand, 'd.M.yyyy HH:mm')) as max_stand FROM "test"."test-parquet" group by datum limit 100; 
+
+# WITH
+#   test_casted AS (
+#    SELECT
+#    DISTINCT *
+#    , CAST(parse_datetime(stand, 'd.M.yyyy HH:mm') AS timestamp) stand_timestamp
+#    , CAST(trim(coalesce(element_at(split(stunde, '-'),1),stunde)) AS integer) stunde_start
+#    , CAST(trim(coalesce(element_at(split(stunde, '-'),2),stunde)) AS integer) stunde_ende
+#    FROM
+#      "test"."test-parquet"
+# ) 
+# , test_max AS (
+#    SELECT
+#      datum
+#    , max(stand_timestamp) stand_max
+#    FROM
+#      test_casted
+#    GROUP BY datum
+# ) 
+# SELECT test_casted.*, (test_casted.stunde_ende + 1 - test_casted.stunde_start) AS stunde_anzahl
+# FROM
+#   (test_casted
+# LEFT JOIN test_max ON (test_casted.datum = test_max.datum))
+# WHERE (stand_max = stand_timestamp)
 
 
 def parse_dsb_html_file(filename):
@@ -28,13 +67,30 @@ def parse_dsb_html_file(filename):
     header = tables[0].iat[0, -1]
     last_update = re.split('[Ss]tand:', header)
 
-    tables[1].insert(0, "Datum", date)
     tables[1].insert(0, "Wochentag", weekday)
     tables[1].insert(len(tables[1].columns), "Stand", last_update[1].strip())
 
-    return tables[1]
+    # Cast all items to string to avoid conversion errors on columns containing partially integers
+    result = tables[1].astype("string")
 
-path = "./temp/20241010-050032_plans/"
-results = [parse_dsb_html_file(os.path.join(path, filename)) for filename in os.listdir(path)]
+    result.insert(0, "Datum", date)
 
-print(pd.concat(results, ignore_index=True, sort=False).sort_values(['Datum', 'Klasse(n)'], ascending = [True, True]))
+    return result
+
+download_path = "./temp/download/"
+for path in os.listdir(download_path):
+    results = []
+    for filename in os.listdir(os.path.join(download_path, path)):
+        try:
+            results.append(parse_dsb_html_file(os.path.join(download_path, path, filename)))
+        except Exception as ex:
+            print("ERROR: Could not read {}\n{}\n{}".format(filename, ex, traceback.format_exc()))
+
+    if len(results):
+        parquet_fullpath = os.path.join("./temp/parquet/", "{}.gzip".format(path))
+        df = pd.concat(results, ignore_index=True, sort=False)
+        print("\nTypes of data after conversion:\n", df.dtypes)
+        df.to_parquet(parquet_fullpath, engine='fastparquet', compression='gzip')
+        print("SUCCESS: Saved parquet {}".format(parquet_fullpath))
+    else:
+        print("WARNING: Could not find data for {}".format(path))
